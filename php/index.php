0b3103945431ce8645328a3d9c37c3bce56c7b13
ananthmysore <ananthj87@gmail.com>
2015-05-19 12:01:52 +0530

    Commit No.60 - Applied Kedage font to all the headings and a items

--- php/index.php
+++ php/index.php
@@ -1,20 +1,41 @@
-<?php include("header.php");	?>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/intermediate/font-awesome.min.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/intermediate/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="css/intermediate/style.css"> <!-- Resource style -->
+	<link rel="shortcut icon" href="img/favicon.png">	
+	<script src="js/intermediate/modernizr.js"></script> <!-- Modernizr -->
+	<title>ಆಕರಭಾರತಿ</title>
+
+
+	
+</head>
+<body>
+	<header class="cd-main-header">
+		<a class="cd-logo" href="../index.html"><i class="fa fa-home fa-2x"></i></a>
+
+		<ul class="cd-header-buttons">
+			<li><a class="cd-search-trigger" href="#cd-search">Search<span></span></a></li>
+			<li><a class="cd-nav-trigger" href="#cd-primary-nav">Menu<span></span></a></li>
+		</ul> <!-- cd-header-buttons -->
+	</header>
+
 <?php include("nav.php"); ?>
 	<main class="cd-main-content">
 		<div id="intro_intermediate">
 			<div class="intro-text">
 				<div class="container">
 					<div class="row">
-					
-						
-					<div class="col-md-12">
-						<div class="brand">
-							<h1><a href="index.html"> ಆಧುನಿಕ ಕರ್ನಾಟಕದ ಬೌದ್ಧಿಕ ಇತಿಹಾಸ</a></h1>
-							<div class="line-spacer"></div>
-							<p><span>ಭಾರತದ ಬೌದ್ಧಿಕ ಸಂಸ್ಕೃತಿಗೊಂದು ದರ್ಪಣ</span></p><br />
+						<div class="col-md-12">
+							<div class="brand">
+								<h1><a href="index.html"> ಆಧುನಿಕ ಕರ್ನಾಟಕದ ಬೌದ್ಧಿಕ ಇತಿಹಾಸ</a></h1>
+								<div class="line-spacer"></div>
+								<p><span>ಭಾರತದ ಬೌದ್ಧಿಕ ಸಂಸ್ಕೃತಿಗೊಂದು ದರ್ಪಣ</span></p><br />
+							</div>
 						</div>
-						
-					</div>
 					</div>
 				</div>
 		 	</div>
