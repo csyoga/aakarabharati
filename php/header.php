89f0d1befdec9975f068501643b2a4e99c99b7ef
ananthmysore <ananthj87@gmail.com>
2015-05-28 17:48:20 +0530

    Commit No.63 - Removed unwanted images and files and added Volumes to .gitignore

--- php/header.php
+++ php/header.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" href="css/intermediate/font-awesome.min.css"> <!-- CSS reset -->
 	<link rel="stylesheet" href="css/intermediate/reset.css"> <!-- CSS reset -->
 	<link rel="stylesheet" href="css/intermediate/style.css"> <!-- Resource style -->
-	<link rel="shortcut icon" href="img/favicon.png">	
+	<link rel="shortcut icon" href="img/favicon.ico">	
 	<script src="js/intermediate/modernizr.js"></script> <!-- Modernizr -->
 	<title>ಆಕರಭಾರತಿ</title>
 </head>
