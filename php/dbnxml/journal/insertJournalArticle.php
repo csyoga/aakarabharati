808cde22777f81f6a6064c0f225dd3a6b43edb9c
suresh07 <soori_rozee@yahoo.com>
2015-07-31 16:42:11 +0530

    Commit No. 8 small correction in insertion

--- php/dbnxml/journal/insertJournalArticle.php
+++ php/dbnxml/journal/insertJournalArticle.php
@@ -3,43 +3,50 @@
 	$database = $argv[2];
 	$user = $argv[3];
 	$password = $argv[4];
-	$journalID = $argv[5];
-	
+		
 	$db = mysql_connect($host, $user, $password) or die("Not connected to database");
 	$rs = mysql_select_db($database, $db) or die("No Database");
 	mysql_query("set names utf8");
-		
-	file_exists('journal-' . $journalID . '.xml') ? $xmlObj = simplexml_load_file('journal-' . $journalID . '.xml') : exit("Failed to open journal-" . $journalID . ".xml. \n");
 	
-	foreach($xmlObj->volume as $volume)
+	$result = mysql_query("SELECT id FROM journaldetails order by id");
+	while($row = mysql_fetch_array($result))
 	{
-		$vnum = $volume['vnum'];
-		foreach($volume->part as $part)
+		$journalID = $row['id'];
+		file_exists('journal-' . $journalID . '.xml') ? $xmlObj = simplexml_load_file('journal-' . $journalID . '.xml') : exit("Failed to open journal-" . $journalID . ".xml. \n");
+	
+		foreach($xmlObj->volume as $volume)
 		{
-			$prevPage = '';
-			$count = 0;
-			$pnum = $part['pnum'];
-			$year = $part['year'];
-			$month = $part['month'];
-			$info = $part['info'];
-			foreach($part->entry as $entry)
+			$vnum = $volume['vnum'];
+			foreach($volume->part as $part)
 			{
-				$title = addslashes($entry->title);
-				$feature = addslashes($entry->feature);
-				$page = $entry->page;
-				$authorString = '';
-				
-				foreach($entry->allauthors->author as $author)
+				$prevPage = '';
+				$count = 0;
+				$pnum = $part['pnum'];
+				$year = $part['year'];
+				$month = $part['month'];
+				$info = $part['info'];
+				foreach($part->entry as $entry)
 				{
-					$authorType = $author['type'];
-					$authorString .= $authorType . ':' . $author . ';'; 
+					$title = addslashes($entry->title);
+					$feature = addslashes($entry->feature);
+					$page = $entry->page;
+					$array = array();
+					$authors = array();
+					$authorJson = '';
+					
+					foreach($entry->allauthors as $author)
+					{
+						$array['name'] = (string)$author->author;
+						$array['type'] = (string)$author->author['type'];
+						array_push($authors, $array);
+					}
+					$authorJson = json_encode($authors, JSON_UNESCAPED_UNICODE);
+					//~ echo $authorJson; exit();
+					(strcmp($page, $prevPage) == 0 ) ? ($titleid = 'id_' . $journalID . '_' . $vnum . '_' .$pnum . '_' . $page . '_' . ++$count) : ($titleid = 'id_' . $journalID . '_' . $vnum . '_' .$pnum . '_' . $page . '_0' AND $count = 0);
+					$prevPage =  $page;
+					$query = "INSERT INTO article VALUES('$journalID', '$vnum', '$pnum', '$year', '$month', '$title', '$feature', '$page', '$authorJson', '$info', '$titleid')";
+					mysql_query($query) or die("Query Problem" . mysql_error());
 				}
-				
-				$authorString = preg_replace('/;$/', '', $authorString);
-				(strcmp($page, $prevPage) == 0 ) ? ($titleid = 'id_' . $journalID . '_' . $vnum . '_' .$pnum . '_' . $page . '_' . ++$count) : ($titleid = 'id_' . $journalID . '_' . $vnum . '_' .$pnum . '_' . $page . '_0' AND $count = 0);
-				$prevPage =  $page;
-				$query = "INSERT INTO article VALUES('$journalID', '$vnum', '$pnum', '$year', '$month', '$title', '$feature', '$page', '$authorString', '$titleid')";
-				mysql_query($query) or die("Query Problem" . mysql_error());
 			}
 		}
 	}
--- php/dbnxml/journal/insertJournalDetails.php
+++ php/dbnxml/journal/insertJournalDetails.php
@@ -8,7 +8,7 @@
 	$rs = mysql_select_db($database, $db) or die("No Database");
 	mysql_query("set names utf8");
 	
-	mysql_query("DROP TABE IF EXISTS journaldetails");
+	mysql_query("DROP TABLE IF EXISTS journaldetails");
 	mysql_query("CREATE TABLE journaldetails (id varchar(5), title varchar(100), period varchar(20), details varchar(300), primary key(id)) ENGINE=MyISAM character set utf8 collate utf8_general_ci");
 	
 	file_exists('journals_details.xml') ? $xmlObj = simplexml_load_file('journals_details.xml') : exit("Failed to open journals_details.xml");
@@ -21,6 +21,6 @@
 		$period = $journal->period;
 		$details = addslashes($journal->details);
 		$query = "INSERT INTO journaldetails VALUES('$id', '$title', '$period', '$details')";
-		mysql_query($query) or die("Query Problem" . mysql_error());
+		mysql_query($query) or die("Query Problem \n" . mysql_error());
 	}
 ?>
