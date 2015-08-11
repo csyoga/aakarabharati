2b4947f258e2e6e1e13e8cf69589c77c88ae1a91
suresh07 <soori_rozee@yahoo.com>
2015-08-11 15:54:02 +0530

    Commit No. 48 - small correction in files remover

--- php/bookreader/templates/filesRemover.php
+++ php/bookreader/templates/filesRemover.php
@@ -1,6 +1,6 @@
 <?php
 	$jpg = 'find ../../../Volumes/jpg/journals/1/ -mmin +10 -type f -name "*.jpg" -exec rm {} \;';
 	exec($jpg);
-	$tif = 'find "../../../Volumes/tif/journals/ -mmin +10 -type f -name "*.tif" -exec rm {} \;';
+	$tif = 'find ../../../Volumes/tif/journals/ -mmin +10 -type f -name "*.tif" -exec rm {} \;';
 	exec($tif);
 ?>
