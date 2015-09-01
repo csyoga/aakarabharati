555c8bf5d7bc2e4855d16fd8554a9c149c8d07a6
suresh07 <soori_rozee@yahoo.com>
2015-09-01 18:11:05 +0530

    Commit No. 69 - Books insertion script added

---
+++ dbnxml/generate.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+host="localhost"
+db="akbi"
+usr="root"
+pwd='mysql'
+
+echo "CREATE DATABASE IF NOT EXISTS $db CHARACTER SET utf8 COLLATE utf8_general_ci;" | /usr/bin/mysql -u$usr -p$pwd
+echo "DROP TABLE IF EXISTS journals; CREATE TABLE journals(journalid varchar(5), volume varchar(3), part varchar(10), year varchar(10),  month varchar(6), title varchar(500), feature varchar(100), page varchar(40), authorname varchar(1000), info varchar(300), titleid varchar(50), primary key(titleid)) ENGINE=MyISAM character set utf8 collate utf8_general_ci" | /usr/bin/mysql -u$usr -p$pwd $db
+echo "DROP TABLE IF EXISTS books; CREATE TABLE books(bookid varchar(5), sstitle varchar(500),level varchar(10), page varchar(30), title varchar(1000),  authorname varchar(1000)) ENGINE=MyISAM character set utf8 collate utf8_general_ci" | /usr/bin/mysql -u$usr -p$pwd $db
+
+php insertJournalDetails.php $host $db $usr $pwd
+php insertJournalArticle.php $host $db $usr $pwd
+php insertBooksDetail.php $host $db $usr $pwd
+php insertBookArticle.php $host $db $usr $pwd
+
+
+
+#~ Removing Journal entries having id's 006 - 009 and 011 
+echo "DELETE FROM journals WHERE journalid REGEXP '00[6-9]' OR journalid = '011'" | /usr/bin/mysql -u$usr -p$pwd $db
+
+#~ sudo chown -R www-data:www-data Volumes
+#~ sudo chmod 777 php/bookreader/temples/apache.manifest
+#~ sudo chown -R www-data:www-data Volumes
